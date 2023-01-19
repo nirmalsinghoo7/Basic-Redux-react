c5cc3461e7b6c7fe38d23eaa2486527a00ccbfef
Nirmal Singh <nirmalpunjabioo7@gmail.com>
2023-01-19 20:09:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Shop from "./components/Shop";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+
+      <Shop />
+      </div>
+    </>
+  );
+}
+
+export default App;
